befe66e0a840fa0a2aba88d54d4936377eb2eee3
Saiprathap3 <128614840+Saiprathap3@users.noreply.github.com>
2023-05-02 13:42:50 +0530

    Update exp 2.c

--- exp 2.c
+++ exp 2.c
@@ -1,45 +1,58 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+#define MAX_FRAMES 10
 
 int main() {
-    int pages[] = {1, 2, 3, 4, 1, 2, 5, 1, 2, 3, 4, 5}; // Test case
-    int n = sizeof(pages) / sizeof(pages[0]);
-    int capacity = 3; // Number of page frames
-    int frames[capacity];
-    int page_faults = 0;
-    int oldest_page = 0;
+    int n, pages[MAX_FRAMES], frames[MAX_FRAMES], faults = 0;
+    printf("Enter the number of page frames: ");
+    scanf("%d", &n);
+    printf("Enter the page reference sequence (separated by spaces): ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &pages[i]);
+    }
 
+    // Initialize all frames to -1 (empty)
+    for (int i = 0; i < MAX_FRAMES; i++) {
+        frames[i] = -1;
+    }
+
+    // Process each page reference
     for (int i = 0; i < n; i++) {
         int page = pages[i];
-        int found = 0;
+        int hit = 0;
 
-        for (int j = 0; j < capacity; j++) {
+        // Check if page is already in a frame
+        for (int j = 0; j < MAX_FRAMES; j++) {
             if (frames[j] == page) {
-                found = 1;
+                hit = 1;
                 break;
             }
         }
 
-        // If page is not in a frame, replace the oldest page with the new page
-        if (!found) {
-            frames[oldest_page] = page;
-            oldest_page = (oldest_page + 1) % capacity;
-            page_faults++;
+        // If page is not in any frame, replace the oldest frame
+        if (!hit) {
+            int oldest = 0;
+            for (int j = 1; j < n; j++) {
+                if (frames[j] == -1 || frames[j] == frames[oldest]) {
+                    oldest = j;
+                }
+            }
+            frames[oldest] = page;
+            faults++;
         }
 
-        // Print the current state of the page frames
-        printf("Page %d: ", page);
-        for (int j = 0; j < capacity; j++) {
-            if (frames[j] == 0) {
-                printf("- ");
-            } else {
+        // Print the current frame state
+        printf("%d: ", page);
+        for (int j = 0; j < n; j++) {
+            if (frames[j] != -1) {
                 printf("%d ", frames[j]);
+            } else {
+                printf("- ");
             }
         }
         printf("\n");
     }
 
-    printf("Total page faults: %d\n", page_faults);
-
+    printf("Total page faults: %d\n", faults);
     return 0;
 }
